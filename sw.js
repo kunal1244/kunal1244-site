f289684b81b50e2b97b7733377dcc346efbcc6ca
Kunal <kunaldas1244@gmail.com>
2020-09-27 01:04:09 +0530

    update resume link

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bc3adb76a2eb76c54e3c.js"
+    "url": "webpack-runtime-08eb92dd020dca6028e1.js"
   },
   {
     "url": "styles.69c8303230227041b58c.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "02375a609af477bd21a500dff28ade46"
+    "revision": "dfbe1d586e2050bd8d7df3863b3fc4ad"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
